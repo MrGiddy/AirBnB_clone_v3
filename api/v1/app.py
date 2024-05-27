ce84f68a819cddfef89ab88b7232f1182f533862
Matumbai Binale <binalematumbai@gmail.com>
2024-05-27 05:11:53 +0300

    (rest_api task 3): Return the status of the API

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+"""Instantiates the flask application"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def call_storage_dot_close(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    if getenv('HBNB_API_HOST'):
+        app.run(
+            host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
+    else:
+        app.run(host='0.0.0.0', port=5000, threaded=True)
