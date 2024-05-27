2a57298d24afc70214008a208ec99b89c7fa5403
Matumbai Binale <binalematumbai@gmail.com>
2024-05-27 14:49:08 +0300

    (rest_api task 5): Create a handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Instantiates the HBNB flask application"""
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask
+from flask import jsonify
+from models import storage
 from os import getenv
 
 
@@ -17,11 +18,17 @@ def call_storage_dot_close(exception=None):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(e):
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     if getenv('HBNB_API_HOST'):
         app.run(
             host=getenv('HBNB_API_HOST'),
             port=getenv('HBNB_API_PORT'),
+            debug=True,
             threaded=True)
     else:
-        app.run(host='0.0.0.0', port=5000, threaded=True)
+        app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
