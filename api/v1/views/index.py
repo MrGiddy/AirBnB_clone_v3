0c3b145170feb51b5afd92c18ebb98c90437e1f4
Matumbai Binale <binalematumbai@gmail.com>
2024-05-27 14:29:13 +0300

    (rest_api task 4): Create an endpoint to retrieve the number of objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,23 @@
 """Defines views for the HBNB-REST API"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     """Returns the OK status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of objects by type"""
+    classes = {
+        "amenities": "Amenity", "cities": "City", "places": "Place",
+        "reviews": "Review", "states": "State", "users": "User"}
+    collection = {}
+    for k, v in classes.items():
+        type_count = {k: storage.count(v)}
+        collection.update(type_count)
+    return jsonify(collection)
